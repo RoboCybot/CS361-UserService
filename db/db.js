e4f5c85124cc93713f1b65b40a538ee2c6c383c5
Prismo <willclarkmusic@gmail.com>
2025-11-23 14:07:43 -0800

    removed unused code in db.js

--- db/db.js
+++ db/db.js
@@ -7,11 +7,6 @@ try {
     "CREATE TABLE IF NOT EXISTS users (userId INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL, phoneNumber TEXT NOT NULL, avatarURL TEXT DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png', userBio TEXT , mfaToken TEXT, createdAt TEXT DEFAULT CURRENT_TIMESTAMP, updatedAt TEXT DEFAULT CURRENT_TIMESTAMP )"
   ).run();
 
-  // db.exec("DROP TABLE users");
-
-  // db.exec(
-  //   "INSERT into users (username, password, phoneNumber) VALUES ('testuser', 'password123', '123-456-7890');"
-  // );
 } catch (error) {
   console.error("Error creating users table:", error);
 }
